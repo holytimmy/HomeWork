e598a63f6c6917cb1b672dd12bb1c152b0d6bb63
Iryna <i.zarytska@godeltech.com>
2022-08-26 15:47:36 +0300

    homework 6.3 работает

---
+++ OOP/6.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6._3
+{
+    static class MyClass
+    {
+        public static void SortMethod(this int []array)
+        {
+            Array.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]+" ");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Требуется: создать расширяющий метод для целочисленного массива, который сортирует элементы 
+массива по возрастанию. */
+
+            int[] array = { 5, 8, 6, 5, 3 };
+            array.SortMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
