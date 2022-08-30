cb9806680998ec352e4fbfa6c45de8c41a0c5a96
Iryna <i.zarytska@godeltech.com>
2022-08-30 14:24:51 +0300

    homework 7; 7.2

---
+++ OOP/7.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2
+{
+    struct Notebook
+    {
+        public string model;
+        public string production;
+        public int price;
+        public Notebook(string model, string production, int price)
+        {
+            this.model = model;
+            this.production = production;
+            this.price = price;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Notebook {model}, {production}, {price}$");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Создайте структуру с именем - Notebook. 
+Поля структуры: модель, производитель, цена. 
+В структуре должен быть реализован конструктор для инициализации полей и метод для вывода 
+содержимого полей на экран. */
+
+            Notebook notebook = new Notebook();
+            notebook.model = "L223";
+            notebook.production = "Asus";
+            notebook.price = 580;
+
+            notebook.Show();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _7
+{
+    struct Train
+    {
+        string station;
+        string numberTrain;
+        string timeDeparture;
+        public string Station { get; set; }
+        public string NumberTrain { get; set; }
+        public string NumberDeparture { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Требуется: Описать структуру с именем Train, содержащую следующие поля: название пункта 
+назначения, номер поезда, время отправления. 
+Написать программу, выполняющую следующие действия: 
+- ввод с клавиатуры данных в массив, состоящий из восьми элементов типа Train (записи должны быть
+упорядочены по номерам поездов);
+- вывод на экран информации о поезде, номер которого введен с клавиатуры (если таких поездов нет,
+вывести соответствующее сообщение).*/
+
+            Train[] transport = new Train[8];
+
+            for (int i = 0; i < transport.Length; i++)
+            {
+                Console.WriteLine($"enter a name of train {i + 1}:");
+                transport[i].Station = Convert.ToString(Console.ReadLine());
+                Console.WriteLine($"enter # of train {i + 1}:");
+                transport[i].NumberTrain = Convert.ToString(Console.ReadLine());
+                Console.WriteLine($"enter departure time of train {i + 1}:");
+                transport[i].NumberDeparture = Convert.ToString(Console.ReadLine());
+            }
+
+            Console.WriteLine("enter number of train:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0 && number < 8)
+            {
+                int i = number - 1;
+                Console.WriteLine($"Station {transport[i].Station}, number{transport[i].NumberTrain},time departure{transport[i].NumberDeparture}");
+            }
+            else
+            {
+                Console.WriteLine("don'n number of train");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
