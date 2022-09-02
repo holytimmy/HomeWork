e18a396895a75e9f9dacd6f82f04dda57ac297bb
Iryna <i.zarytska@godeltech.com>
2022-09-02 15:56:46 +0300

    8 вопрос есть

--- OOP/8/Program.cs
+++ OOP/8/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _8
 {
@@ -13,8 +9,17 @@ namespace _8
             /*Реализуйте программу, которая будет принимать от пользователя дату его рождения и выводить 
 количество дней до его следующего дня рождения. */
 
+            //Console.WriteLine("Enter date your next birthday:");
+            //var dateString = Convert.ToString(Console.ReadLine());// "5/1/2008 8:30:52 AM";
+            //DateTime dateBirthday = DateTime.Parse(dateString,System.Globalization.CultureInfo.InvariantCulture);
 
+            DateTime dateBirthday = new DateTime(2022, 11, 15, 8, 0, 15);
+            DateTime dateNow = DateTime.Now;
 
+            TimeSpan interval = dateBirthday - dateNow;
+            Console.WriteLine("There are " + interval.Days + " days left until the birthday");
+
+            Console.ReadKey();
         }
     }
 }
